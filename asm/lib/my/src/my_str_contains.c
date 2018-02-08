0ba7361bee642df444635574fc56b158f5247132
Frabel Julian <julian.frabel@epitech.eu>
2018-02-08 20:38:39 +0100

    Added my_str_contains to my

---
+++ asm/lib/my/src/my_str_contains.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2017
+** CPE_corewar_2017
+** File description:
+** return the index if the str contains the value, -1 otherwise
+*/
+
+#include "my.h"
+
+int my_str_contains(char c, char *str)
+{
+	for (int i = 0; str[i]; i++) {
+		if (str[i] == c)
+			return (i);
+	}
+	return (-1);
+}
