6ca987ad86f298367c35d8a27a84677680c5a787
Frabel Julian <julian.frabel@outlook.fr>
2018-02-09 18:06:50 +0100

    * Started adding unit tests

---
+++ asm/tests/src/tests_detect_types.c
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2017
+** CPE_corewar_2017
+** File description:
+** Test the detect_type functions
+*/
+
+/* File created the 09/02/2018 at 18:05:45 by julian.frabel@epitech.eu */
+
+#include <criterion/criterion.h>
+#include "my.h"
