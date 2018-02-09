66a21a3de14b26348d104f1222f9cc0fb9fb58cb
Frabel Julian <julian.frabel@outlook.fr>
2018-02-09 18:28:16 +0100

    Added tests for is label_set

--- asm/lib/mylist/src/dict_add.c
+++ asm/lib/mylist/src/dict_add.c
@@ -12,6 +12,8 @@ static int my_strlen(char const *str)
 {
 	int i = 0;
 
+	if (str == NULL)
+		return (0);
 	for (; str[i]; i++);
 	return (i);
 }
--- asm/tests/src/tests_detect_types.c
+++ asm/tests/src/tests_detect_types.c
@@ -24,6 +24,7 @@ Test(is_register, test_the_is_register_function)
 	cr_expect(!is_register(""));
 	cr_expect(!is_register("A"));
 	cr_expect(!is_register("1r"));
+	cr_expect(!is_register(NULL));
 }
 
 Test(is_direct, test_the_is_direct_function)
@@ -39,6 +40,7 @@ Test(is_direct, test_the_is_direct_function)
 	cr_expect(!is_direct(""));
 	cr_expect(!is_direct("A"));
 	cr_expect(!is_direct("1r"));
+	cr_expect(!is_direct(NULL));
 }
 
 Test(is_indirect, test_the_is_indirect_function)
@@ -46,9 +48,23 @@ Test(is_indirect, test_the_is_indirect_function)
 	cr_expect(is_indirect("1"));
 	cr_expect(is_indirect("18"));
 	cr_expect(is_indirect(":label"));
+	cr_expect(is_indirect("-1"));
 	cr_expect(!is_indirect(""));
 	cr_expect(!is_indirect("%5"));
 	cr_expect(!is_indirect("notalabel:"));
 	cr_expect(!is_indirect("A"));
 	cr_expect(!is_indirect("r1"));
+	cr_expect(!is_indirect(NULL));
+}
+
+Test(is_label_set, test_if_str_is_a_label_set)
+{
+	cr_expect(is_label_set("label:"));
+	cr_expect(is_label_set("live:"));
+	cr_expect(is_label_set("04j_z:"));
+	cr_expect(!is_label_set(""));
+	cr_expect(!is_label_set("lAbel:"));
+	cr_expect(!is_label_set("r1"));
+	cr_expect(!is_label_set(":label"));
+	cr_expect(!is_label_set(NULL));
 }
