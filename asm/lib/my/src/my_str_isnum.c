115addde2042337f1eff26bcdad2512537937ec6
Frabel Julian <julian.frabel@outlook.fr>
2018-02-09 18:16:19 +0100

    Added test to is_direct and fixed a bug where %-1 was not taken as a direct

--- asm/lib/my/src/my_str_isnum.c
+++ asm/lib/my/src/my_str_isnum.c
@@ -10,7 +10,7 @@ int my_str_isnum(char const *str)
 	if (str[0] == '\0')
 		return (1);
 	for (int i = 0; str[i] != '\0'; i++) {
-		if (str[i] < '0' || str[i] > '9')
+		if (str[i] < '0' || str[i] > '9' || str[i] == '-')
 			return (0);
 	}
 	return (1);
--- asm/tests/src/tests_detect_types.c
+++ asm/tests/src/tests_detect_types.c
@@ -9,3 +9,34 @@
 
 #include <criterion/criterion.h>
 #include "my.h"
+#include "asm.h"
+
+Test(is_register, test_the_is_register_function)
+{
+	cr_expect(is_register("r1"));
+	cr_expect(is_register("r16"));
+	cr_expect(is_register("r7"));
+	cr_expect(!is_register("r0"));
+	cr_expect(!is_register("r17"));
+	cr_expect(!is_register("r1a"));
+	cr_expect(!is_register("R1"));
+	cr_expect(!is_register("ra"));
+	cr_expect(!is_register(""));
+	cr_expect(!is_register("A"));
+	cr_expect(!is_register("1r"));
+}
+
+Test(is_direct, test_the_is_direct_function)
+{
+	cr_expect(is_direct("%1"));
+	cr_expect(is_direct("%16"));
+	cr_expect(is_direct("%:label"));
+	cr_expect(is_direct("%-1"));
+	cr_expect(!is_direct("r0"));
+	cr_expect(!is_direct("aer"));
+	cr_expect(!is_direct("1"));
+	cr_expect(!is_direct("%notlabel:"));
+	cr_expect(!is_direct(""));
+	cr_expect(!is_direct("A"));
+	cr_expect(!is_direct("1r"));
+}
