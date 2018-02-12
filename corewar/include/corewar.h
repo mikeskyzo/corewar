80b151cb3c29d72f6fc1f2baa1598314d01df111
Frabel Julian <julian.frabel@outlook.fr>
2018-02-12 11:00:32 +0100

    Added vm_t and create vm function

--- corewar/include/corewar.h
+++ corewar/include/corewar.h
@@ -9,6 +9,9 @@
 # define COREWAR_H_
 
 #include "op.h"
+#include "mylist.h"
+
+#define BYTE char
 
 struct champ_s
 {
@@ -18,6 +21,16 @@ struct champ_s
 };
 
 typedef struct champ_s champ_t;
+
+struct vm_s
+{
+	linked_list_t *programs;
+	BYTE ram[MEM_SIZE];
+	int current_cycle;
+	int cycle_to_die;
+};
+
+typedef struct vm_s vm_t;
 
 extern champ_t champ_tab[];
 
