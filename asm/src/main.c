217878005de185d2d4c82cb78ee14d5d44e7bb0b
Frabel Julian <julian.frabel@outlook.fr>
2018-02-13 11:20:21 +0100

    Added O_Creat i main

--- asm/src/main.c
+++ asm/src/main.c
@@ -25,7 +25,7 @@ void write_error_message(assembly_data_t *data)
 int main(int ac, char **av)
 {
 	int fd = (ac >= 1) ? open(av[1], O_RDONLY) : -1;
-	int write_fd = (ac >= 2) ? open(av[2], O_RDWR) : -1;
+	int write_fd = (ac >= 2) ? open(av[2], O_CREAT | O_RDWR) : -1;
 	assembly_data_t data = {{COREWAR_EXEC_MAGIC, {0}, 0, {0}}, 0, {0}, 0};
 
 	if (fd == -1 || write_fd == -1)
