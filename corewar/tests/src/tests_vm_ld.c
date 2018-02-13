8548a1de7a22fd39f64a14b40d3e06c70a377240
Frabel Julian <julian.frabel@outlook.fr>
2018-02-13 18:25:41 +0100

    Added unit tests for indirect ld

--- corewar/tests/src/tests_vm_ld.c
+++ corewar/tests/src/tests_vm_ld.c
@@ -14,19 +14,19 @@
 Test(vm_ld, correct_register, .timeout=10)
 {
 	vm_t *vm = create_vm();
-	champ_t *champion = create_blank_champion();
+	champ_t *champ = create_blank_champion();
 	byte_t instruction[2 + IND_SIZE + 1] = {0x02, 0xd0, 0x00, 0x01, 0x03};
 
-	if (vm == NULL || champion == NULL)
+	if (vm == NULL || champ == NULL)
 		cr_assert_fail();
 	else {
-		champion->pc = instruction;
-		vm_ld(vm, instruction, champion);
-		cr_expect(champion->registers[2 * REG_SIZE + 0] == 0xd0);
-		cr_expect(champion->registers[2 * REG_SIZE + 1] == 0x00);
-		cr_expect(champion->registers[2 * REG_SIZE + 2] == 0x01);
-		cr_expect(champion->registers[2 * REG_SIZE + 3] == 0x03);
+		champ->pc = instruction;
+		vm_ld(vm, instruction, champ);
+		cr_expect(champ->registers[2 * REG_SIZE + 0] == (byte_t)0xd0);
+		cr_expect(champ->registers[2 * REG_SIZE + 1] == (byte_t)0x00);
+		cr_expect(champ->registers[2 * REG_SIZE + 2] == (byte_t)0x01);
+		cr_expect(champ->registers[2 * REG_SIZE + 3] == (byte_t)0x03);
 		free(vm);
-		free(champion);
+		free(champ);
 	}
 }
