c1c5babf188f46aa318970e45d210e55160ff7ba
Frabel Julian <julian.frabel@outlook.fr>
2018-02-13 14:09:38 +0100

    * renamed live.c to vm_live.c + added vm_ld file

---
+++ corewar/src/vm_functions/vm_ld.c
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2017
+** CPE_corewar
+** File description:
+** ld function of the vm
+*/
+
+/* File created the 13/02/2018 at 14:08:58 by julian.frabel@epitech.eu */
+
+#include "corewar.h"
+#include "my.h"
