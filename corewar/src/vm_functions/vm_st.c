2dbbedf13eab2303bd38e8aa3b8de02d68b72379
Frabel Julian <julian.frabel@outlook.fr>
2018-02-14 11:02:42 +0100

    Moved function in correct file

--- corewar/src/vm_functions/vm_st.c
+++ corewar/src/vm_functions/vm_st.c
@@ -6,3 +6,11 @@
 */
 
 /* File created the 14/02/2018 at 10:57:38 by julian.frabel@epitech.eu */
+
+#include "corewar.h"
+#include "my.h"
+
+int vm_st(vm_t *vm, byte_t *instruction_pos, champ_t *champion)
+{
+	return (0);
+}
--- corewar/tests/src/tests_vm_st.c
+++ corewar/tests/src/tests_vm_st.c
@@ -7,10 +7,3 @@
 
 /* File created the 14/02/2018 at 11:00:13 by julian.frabel@epitech.eu */
 
-#include "corewar.h"
-#include "my.h"
-
-int vm_st(vm_t *vm, byte_t *instruction_pos, champ_t *champion)
-{
-	return (0);
-}
