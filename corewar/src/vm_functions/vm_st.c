45b18a70be6fa7d529ab7f5e2a0b2fb774df9bab
Frabel Julian <julian.frabel@outlook.fr>
2018-02-14 11:01:59 +0100

    Started working on the st function

---
+++ corewar/src/vm_functions/vm_st.c
@@ -0,0 +1,8 @@
+/*
+** EPITECH PROJECT, 2017
+** CPE_corewar
+** File description:
+** st function of the vm
+*/
+
+/* File created the 14/02/2018 at 10:57:38 by julian.frabel@epitech.eu */
---
+++ corewar/tests/src/tests_vm_st.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2017
+** CPE_corewar
+** File description:
+** Test the st function
+*/
+
+/* File created the 14/02/2018 at 11:00:13 by julian.frabel@epitech.eu */
+
+#include "corewar.h"
+#include "my.h"
+
+int vm_st(vm_t *vm, byte_t *instruction_pos, champ_t *champion)
+{
+	return (0);
+}
