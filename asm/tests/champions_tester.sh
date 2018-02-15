c50ba137535b4b0da1869f118ebcc7fae3ace5e8
brian <brian.guitteny@epitech.eu>
2018-02-15 15:11:55 +0100

    Updating tests with new compilator

--- asm/tests/champions_tester.sh
+++ asm/tests/champions_tester.sh
@@ -3,10 +3,13 @@ for file in *.s
 do
 	echo "Testing champion $file ..."
 	cp "$file" current.asm
+	cp current.asm current2.asm
 	./asm current.asm
-	../../asm current.asm test
-	diff current.asm.cor test
+	../../asm current2.asm
+	diff current.asm.cor current2.asm.cor
 	rm test
 	rm current.asm
+	rm current2.asm
 	rm current.asm.cor
+	rm current2.asm.cor
 done
