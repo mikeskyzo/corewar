0509a1f35eac6a29fef1c9eb856d25242f8fc3fc
Argann Bay-Eymère <argann.bay-eymere@epitech.eu>
2018-02-16 09:44:36 +0100

    * Fixed header.magix and header.prog_size (reverse endian)

--- corewar/src/get_champ.c
+++ corewar/src/get_champ.c
@@ -13,6 +13,27 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+static int reverse_endian(int val, int size)
+{
+	unsigned int x = 0x76543210;
+	char *checker = (char *)&x;
+	int b0 = (val & 0xff) << 24;
+	int b1 = (val & 0xff00) << 8;
+	int b2 = (val & 0xff0000) >> 8;
+	int b3 = (val & 0xff000000) >> 24;
+	int res = b0 | b1 | b2 | b3;
+
+	if (*checker == 0x76)
+		return (val);
+	switch (size) {
+		case 1: return (val);
+		case 2:	return ((val >> 8) | (val & 0xff) << 8);
+		case 3: return (val);
+		case 4: return (b0 | b1 | b2 | b3);
+	}
+	return (res);
+}
+
 char *get_prog(int size, int fd)
 {
 	char *buffer;
@@ -44,6 +65,8 @@ champ_t read_champ(char *file, int *fd)
 		my_puterror("Fail to read\n");
 		exit(84);
 	}
+	champ.header.magic = reverse_endian(champ.header.magic, 4);
+	champ.header.prog_size = reverse_endian(champ.header.prog_size, 4);
 	return (champ);
 }
 
