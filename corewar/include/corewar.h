68ba114004d10c2c4f3a06498247fe259a2c61ca
Argann Bay-Eymère <argann.bay-eymere@epitech.eu>
2018-02-17 09:46:13 +0100

    * Handled basic errors * Correctly indented header file

--- corewar/include/corewar.h
+++ corewar/include/corewar.h
@@ -6,94 +6,98 @@
 */
 
 #ifndef COREWAR_H_
-# define COREWAR_H_
+	#define COREWAR_H_
 
-#include "op.h"
-#include "mylist.h"
+	#include "op.h"
+	#include "mylist.h"
 
-#define LIVE_STR "The player %d(%s) is alive.\n" /* player number/player name */
-#define WON_STR "The player %d(%s) has won.\n" /* player number/player name */
+	#define LIVE_STR "The player %d(%s) is alive.\n" /* player number/player name */
+	#define WON_STR "The player %d(%s) has won.\n" /* player number/player name */
 
-#define MAX(value1, value2) ((value1 > value2) ? value1 : value2)
-#define MIN(value1, value2) ((value1 > value2) ? value2 : value1)
+	#define MAX(value1, value2) ((value1 > value2) ? value1 : value2)
+	#define MIN(value1, value2) ((value1 > value2) ? value2 : value1)
 
-/* Bits masks */
-#define FIRST_PARAM_MASK	0b11000000
-#define SECOND_PARAM_MASK	0b00110000
-#define THIRD_PARAM_MASK	0b00001100
-#define FOURTH_PARAM_MASK	0b00000011
+	/* Bits masks */
+	#define FIRST_PARAM_MASK	0b11000000
+	#define SECOND_PARAM_MASK	0b00110000
+	#define THIRD_PARAM_MASK	0b00001100
+	#define FOURTH_PARAM_MASK	0b00000011
 
-#define REGISTER_TYPE	0b01
-#define DIRECT_TYPE	0b10
-#define INDIRECT_TYPE	0b11
+	#define REGISTER_TYPE	0b01
+	#define DIRECT_TYPE	0b10
+	#define INDIRECT_TYPE	0b11
 
-#define FIRST_PARAM_TYPE(value) ((value & FIRST_PARAM_MASK) >> 6)
-#define SECOND_PARAM_TYPE(value) ((value & SECOND_PARAM_MASK) >> 4)
-#define THIRD_PARAM_TYPE(value) ((value & THIRD_PARAM_MASK) >> 2)
-#define FOURTH_PARAM_TYPE(value) ((value & FOURTH_PARAM_MASK))
+	#define FIRST_PARAM_TYPE(value) ((value & FIRST_PARAM_MASK) >> 6)
+	#define SECOND_PARAM_TYPE(value) ((value & SECOND_PARAM_MASK) >> 4)
+	#define THIRD_PARAM_TYPE(value) ((value & THIRD_PARAM_MASK) >> 2)
+	#define FOURTH_PARAM_TYPE(value) ((value & FOURTH_PARAM_MASK))
 
-#define SIZEOF_PARAM(type) ((type == REGISTER_TYPE) ? 1 : \
-((type == DIRECT_TYPE) ? DIR_SIZE : IND_SIZE))
+	#define SIZEOF_PARAM(type) ((type == REGISTER_TYPE) ? 1 : \
+	((type == DIRECT_TYPE) ? DIR_SIZE : IND_SIZE))
 
-typedef unsigned char byte_t;
+	typedef unsigned char byte_t;
 
-struct champ_s
-{
-	header_t header;
-	int champion_nb;
-	int nb_next_ins;
-	int nb_cycle_live;
-	char *prog;
-	int nb_prog;
-	int load;
-	byte_t *pc;
-	bool carry;
-	byte_t registers[REG_SIZE * REG_NUMBER];
-};
+	struct champ_s
+	{
+		header_t header;
+		int champion_nb;
+		int nb_next_ins;
+		int nb_cycle_live;
+		char *prog;
+		int nb_prog;
+		int load;
+		byte_t *pc;
+		bool carry;
+		byte_t registers[REG_SIZE * REG_NUMBER];
+	};
 
-typedef struct champ_s champ_t;
+	typedef struct champ_s champ_t;
 
-struct vm_s
-{
-	linked_list_t *programs;
-	byte_t ram[MEM_SIZE];
-	int current_cycle;
-	int cycle_to_die;
-};
+	struct vm_s
+	{
+		linked_list_t *programs;
+		byte_t ram[MEM_SIZE];
+		int current_cycle;
+		int cycle_to_die;
+	};
 
-typedef struct vm_s vm_t;
+	typedef struct vm_s vm_t;
 
-champ_t *read_arg(char**, int);
-champ_t get_champ(char*, int, int);
+	champ_t *read_arg(char**, int);
+	champ_t get_champ(char*, int, int);
 
-void puts_help(void);
+	int load_champ(vm_t *vm, champ_t *champ, int champ_ind, int champ_nb);
+	int load_champ_all(vm_t *vm, champ_t *champ, int champ_nb);
 
-vm_t *create_vm(void);
-champ_t *create_blank_champion(void);
+	void puts_help(void);
 
-/* Accessors */
-champ_t *get_champion_by_number(vm_t *vm, int champion_nb);
-champ_t *get_champion_by_name(vm_t *vm, const char *name);
+	vm_t *create_vm(void);
+	champ_t *create_blank_champion(void);
 
-int get_register_as_int(byte_t *start);
-int get_direct_as_int(byte_t *start);
-int get_indirect_as_int(byte_t *start);
-int get_indirect_value_as_int(int indirect, byte_t *pc);
+	/* Accessors */
+	champ_t *get_champion_by_number	(vm_t *vm, int champion_nb);
+	champ_t *get_champion_by_name	(vm_t *vm, const char *name);
 
-/*
-** Functions vm (mnemonics) prototyped as follow:
-** int vm_FUNCNAME(vm_t *vm, byte_t *instruction_pos, champ_t *champion)
-** vm will be the vm and instruction_pos a pointer to the mnemonic called
-** champion will be the champion that called this instruction
-** the functions will return the size of himself in memory (ex live return 5)
-*/
-int vm_live(vm_t *vm, byte_t *instruction_pos, champ_t *champion);
-int vm_ld(vm_t *vm, byte_t *instruction_pos, champ_t *champion);
-int vm_st(vm_t *vm, byte_t *instruction_pos, champ_t *champion);
-int vm_add(vm_t *vm, byte_t *instruction_pos, champ_t *champion);
-int vm_sub(vm_t *vm, byte_t *instruction, champ_t *champ);
-int vm_and(vm_t *vm, byte_t *instruction_pos, champ_t *champion);
-int vm_or(vm_t *vm, byte_t *instruction_pos, champ_t *champion);
-int vm_xor(vm_t *vm, byte_t *instruction_pos, champ_t *champion);
+	int get_register_as_int		(byte_t *start);
+	int get_direct_as_int		(byte_t *start);
+	int get_indirect_as_int		(byte_t *start);
+	int get_indirect_value_as_int	(int indirect, byte_t *pc);
+
+	/*
+	** Functions vm (mnemonics) prototyped as follow:
+	** int vm_FUNCNAME(vm_t *vm, byte_t *instruction_pos, champ_t *champion)
+	** vm will be the vm and instruction_pos a pointer to the mnemonic
+	** called champion will be the champion that called this instruction
+	** the functions will return the size of himself in memory
+	** (ex live return 5)
+	*/
+	int vm_live	(vm_t *vm, byte_t *instruction_pos, champ_t *champion);
+	int vm_ld	(vm_t *vm, byte_t *instruction_pos, champ_t *champion);
+	int vm_st	(vm_t *vm, byte_t *instruction_pos, champ_t *champion);
+	int vm_add	(vm_t *vm, byte_t *instruction_pos, champ_t *champion);
+	int vm_sub	(vm_t *vm, byte_t *instruction_pos, champ_t *champion);
+	int vm_and	(vm_t *vm, byte_t *instruction_pos, champ_t *champion);
+	int vm_or	(vm_t *vm, byte_t *instruction_pos, champ_t *champion);
+	int vm_xor	(vm_t *vm, byte_t *instruction_pos, champ_t *champion);
 
 #endif
--- corewar/src/load_champ.c
+++ corewar/src/load_champ.c
@@ -14,7 +14,10 @@ static int compute_spacement(champ_t *tab, int champ_nb)
 
 	for (int i = 0; i < champ_nb; i++)
 		tot_size += tab[i].header.prog_size;
-	return (tot_size / champ_nb);
+	if (MEM_SIZE < tot_size)
+		return (-1);
+	else
+		return (tot_size / champ_nb);
 }
 
 static int compute_rampos(champ_t *champ_tab, int champ_ind, int champ_nb)
@@ -22,12 +25,14 @@ static int compute_rampos(champ_t *champ_tab, int champ_ind, int champ_nb)
 	int spacement = compute_spacement(champ_tab, champ_nb);
 	int rampos = 0;
 
+	if (spacement < 0)
+		return (-1);
 	for (int i = 0; i < champ_ind; i++)
 		rampos += spacement + champ_tab[i].header.prog_size;
 	return (rampos);
 }
 
-static void load_in_ram(vm_t *vm, champ_t *champ, int ram_pos)
+static void load_in_ram(vm_t *vm, champ_t *champ, unsigned int ram_pos)
 {
 	for (int i = 0; i < champ->header.prog_size; i++)
 		vm->ram[(ram_pos + i) % MEM_SIZE] = champ->prog[i];
@@ -35,16 +40,23 @@ static void load_in_ram(vm_t *vm, champ_t *champ, int ram_pos)
 
 int load_champ(vm_t *vm, champ_t *champ, int champ_ind, int champ_nb)
 {
-	for (int i = 0; i < champ_nb; i++)
-		load_in_ram(vm, &(champ[i]), \
-compute_rampos(champ, i, champ_nb));
-	return (NULL);
+	int rampos = compute_rampos(champ, champ_ind, champ_nb);
+
+	if (rampos < 0)
+		return (-1);
+	load_in_ram(vm, &(champ[champ_ind]), rampos);
+	return (0);
 }
 
-int load_champ_at(vm_t *vm, champ_t *champ, int champ_ind, int champ_nb)
+int load_champ_all(vm_t *vm, champ_t *champ, int champ_nb)
 {
-	for (int i = 0; i < champ_nb; i++)
-		load_in_ram(vm, &(champ[i]), \
-compute_rampos(champ, i, champ_nb));
-	return (NULL);
+	int rampos = 0;
+
+	for (int i = 0; i < champ_nb; i++) {
+		rampos = compute_rampos(champ, i, champ_nb);
+		if (rampos < 0)
+			return (-1);
+		load_in_ram(vm, &(champ[i]), rampos);
+	}
+	return (0);
 }
