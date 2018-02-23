f12e5caa37dee3a71e0c44f07b62104f168ea5dc
Frabel Julian <julian.frabel@outlook.fr>
2018-02-23 12:12:56 +0100

    Updated ld

--- corewar/src/vm_functions/vm_ld.c
+++ corewar/src/vm_functions/vm_ld.c
@@ -10,42 +10,43 @@
 #include "corewar.h"
 #include "my.h"
 
-static int load_indirect_to_register(byte_t *instruction_pos, champ_t *champ)
+static int load_indirect_to_register(byte_t *ram, champ_t *champ)
 {
 	byte_t *register_start = NULL;
-	byte_t *indirect_start = NULL;
+	int indirect_start = 0;
 	int indirect_value = 0;
 	byte_t register_nb = 0;
 
-	if (instruction_pos == NULL || champ == NULL)
+	if (ram == NULL || champ == NULL)
 		return (-1);
-	register_nb = *(instruction_pos + 2 + IND_SIZE);
+	register_nb = ram[(champ->pc + 2 + IND_SIZE) % MEM_SIZE];
 	if (register_nb < 1 || register_nb > REG_NUMBER)
 		return (-1);
-	indirect_value = get_indirect_as_int(instruction_pos + 2);
+	indirect_value = get_indirect_as_int(ram, champ->pc + 2);
 	indirect_start = champ->pc + indirect_value % IDX_MOD;
 	register_start = &(champ->registers[(register_nb - 1) * REG_SIZE]);
 	for (int i = 0; i < REG_SIZE; i++)
-		register_start[i] = indirect_start[i];
+		register_start[i] = ram[(indirect_start + i) % MEM_SIZE];
 	champ->carry = ((get_register_as_int(register_start) == 0) ? 1 : 0);
 	return (0);
 }
 
-static int load_direct_to_register(byte_t *instruction_pos, champ_t *champ)
+static int load_direct_to_register(byte_t *ram, champ_t *champ)
 {
 	byte_t *register_start = NULL;
-	byte_t *direct_start = NULL;
+	int direct_start = 0;
 	byte_t register_nb = 0;
 
-	if (instruction_pos == NULL || champ == NULL)
+	if (ram == NULL || champ == NULL)
 		return (-1);
-	register_nb = *(instruction_pos + 2 + DIR_SIZE);
+	register_nb = ram[(champ->pc + 2 + DIR_SIZE) % MEM_SIZE];
 	if (register_nb < 1 || register_nb > REG_NUMBER)
 		return (-1);
-	direct_start = (instruction_pos + 2);
+	direct_start = (champ->pc + 2);
 	register_start = &(champ->registers[(register_nb - 1) * REG_SIZE]);
 	for (int i = 1; i <= MIN(REG_SIZE, DIR_SIZE); i++)
-		register_start[REG_SIZE - i] = direct_start[DIR_SIZE - i];
+		register_start[REG_SIZE - i] = \
+ram[(direct_start + DIR_SIZE - i) % MEM_SIZE];
 	champ->carry = ((get_register_as_int(register_start) == 0) ? 1 : 0);
 	return (0);
 }
@@ -56,7 +57,7 @@ int vm_ld(vm_t *vm, byte_t *instruction_pos, champ_t *champion)
 
 	if (vm == NULL || instruction_pos == NULL || champion == NULL)
 		return (-1);
-	types = *(instruction_pos + 1);
+	types = vm->ram[(champion->pc + 1) % MEM_SIZE];
 	if (FIRST_PARAM_TYPE(types) == DIRECT_TYPE)
 		load_direct_to_register(instruction_pos, champion);
 	else
