a337915907eceb93df39108073941823b27f12b2
Frabel Julian <julian.frabel@outlook.fr>
2018-02-23 13:16:59 +0100

    Updated xor

--- corewar/src/vm_functions/vm_xor.c
+++ corewar/src/vm_functions/vm_xor.c
@@ -10,11 +10,12 @@
 #include "my.h"
 #include "corewar.h"
 
-static void set_register_value(int result, champ_t *champ, byte_t *instruction)
+static void set_register_value(int result, champ_t *champ, byte_t *ram)
 {
-	byte_t types = *(instruction + 1);
-	byte_t register_nb = *(instruction + 2 + \
-SIZEOF_PARAM(FIRST_PARAM_TYPE(types)) + SIZEOF_PARAM(SECOND_PARAM_TYPE(types)));
+	byte_t types = ram[(champ->pc + 1) % MEM_SIZE];
+	byte_t register_nb = ram[(champ->pc + 2 + \
+SIZEOF_PARAM(FIRST_PARAM_TYPE(types)) + \
+SIZEOF_PARAM(SECOND_PARAM_TYPE(types))) % MEM_SIZE];
 	byte_t *reg_start = &(champ->registers[(register_nb - 1) * REG_SIZE]);
 
 	if (register_nb > REG_NUMBER)
@@ -25,22 +26,22 @@ SIZEOF_PARAM(FIRST_PARAM_TYPE(types)) + SIZEOF_PARAM(SECOND_PARAM_TYPE(types)));
 	reg_start[REG_SIZE - 1] = (result) & 0xff;
 }
 
-static int get_value(byte_t *instruction, champ_t *champion, int argument)
+static int get_value(byte_t *ram, champ_t *champion, int argument)
 {
-	byte_t types = *(instruction + 1);
-	byte_t *arg = (instruction + 2 + \
+	byte_t types = ram[(champion->pc + 1) % MEM_SIZE];
+	int arg = (champion->pc + 2 + \
 (argument - 1) * SIZEOF_PARAM(FIRST_PARAM_TYPE(types)));
 	byte_t argument_type = ((argument == 1) ? FIRST_PARAM_TYPE(types) : \
 SECOND_PARAM_TYPE(types));
 
-	if (argument_type == REGISTER_TYPE && *arg > REG_NUMBER)
+	if (argument_type == REGISTER_TYPE && ram[arg % MEM_SIZE] > REG_NUMBER)
 		return (0);
 	switch (argument_type) {
 	case REGISTER_TYPE: return (get_register_as_int(\
-&(champion->registers[((*arg) - 1) * REG_SIZE])));
-	case DIRECT_TYPE: return (get_direct_as_int(arg));
+&(champion->registers[(ram[arg % MEM_SIZE] - 1) * REG_SIZE])));
+	case DIRECT_TYPE: return (get_direct_as_int(ram, arg));
 	case INDIRECT_TYPE: return (get_indirect_value_as_int(\
-get_indirect_as_int(arg), champion->pc));
+get_indirect_as_int(ram, arg), ram, champion->pc));
 	default: return (0);
 	}
 }
@@ -50,14 +51,14 @@ int vm_xor(vm_t *vm, byte_t *instruction_pos, champ_t *champion)
 	int value1 = 0;
 	int value2 = 0;
 	int result = 0;
-	byte_t types = *(instruction_pos + 1);
+	byte_t types = vm->ram[(champion->pc + 1) % MEM_SIZE];
 
 	if (vm == NULL || instruction_pos == NULL || champion == NULL)
 		return (-1);
-	value1 = get_value(instruction_pos, champion, 1);
-	value2 = get_value(instruction_pos, champion, 2);
+	value1 = get_value(vm->ram, champion, 1);
+	value2 = get_value(vm->ram, champion, 2);
 	result = value1 ^ value2;
-	set_register_value(result, champion, instruction_pos);
+	set_register_value(result, champion, vm->ram);
 	champion->carry = !(result);
 	return (3 + SIZEOF_PARAM(FIRST_PARAM_TYPE(types)) + \
 SIZEOF_PARAM(SECOND_PARAM_TYPE(types)));
