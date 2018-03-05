749c7a60ade5588a9c2ee4740da33c7b9af1c8e5
Argann Bay-Eymère <argann.bay-eymere@epitech.eu>
2018-03-05 17:39:09 +0100

    * Fixed zjump fx (misdeclared int instead of short)

--- corewar/src/vm_functions/vm_zjump.c
+++ corewar/src/vm_functions/vm_zjump.c
@@ -10,12 +10,13 @@
 
 int vm_zjmp(vm_t *vm, champ_t *champ)
 {
-	int arg = get_indirect_as_int(vm->ram, champ->pc + 1 % MEM_SIZE);
+	short arg = 0;
 
 	if (vm == NULL || champ == NULL)
 		return (-1);
 	if (champ->carry == false)
 		return (3);
+	arg = get_indirect_as_int(vm->ram, (champ->pc + 1) % MEM_SIZE);
 	champ->pc += (arg % IDX_MOD);
 	return (0);
 }
