bc2fef336bb000597157d31e6d55b4d996d3d0d7
Frabel Julian <julian.frabel@outlook.fr>
2018-03-06 19:15:33 +0100

    Added good types to sti

--- corewar/src/vm_functions/vm_sti.c
+++ corewar/src/vm_functions/vm_sti.c
@@ -8,15 +8,35 @@
 #include "corewar.h"
 #include <stdlib.h>
 
+static void get_sec_last(vm_t *vm, champ_t *champ, int *sec, int *last)
+{
+	byte_t types = vm->ram[(champ->pc + 1) % MEM_SIZE];
+	int was_indirect = 1;
+
+	if (SECOND_PARAM_TYPE(types) == REGISTER_TYPE) {
+		*sec = get_register_as_int(&(champ->registers[\
+(vm->ram[(champ->pc + 3) % MEM_SIZE] - 1) * REG_SIZE]));
+		was_indirect = 0;
+	} else
+		*sec = get_indirect_as_int(vm->ram, (champ->pc + 3) % MEM_SIZE);
+	if (THIRD_PARAM_TYPE(types) == REGISTER_TYPE) {
+		*last = get_register_as_int(&(champ->registers[\
+(vm->ram[(champ->pc + 4 + was_indirect) % MEM_SIZE] - 1) * REG_SIZE]));
+	} else
+		*last = get_indirect_as_int(vm->ram, \
+(champ->pc + 4 + was_indirect) % MEM_SIZE);
+}
+
 int vm_sti(vm_t *vm, champ_t *champ)
 {
 	int reg;
 	byte_t *reg_start = NULL;
-	int sec = get_indirect_as_int(vm->ram, (champ->pc + 3) % MEM_SIZE);
-	int last = get_indirect_as_int(vm->ram, (champ->pc + 5) % MEM_SIZE);
+	int sec = 0;
+	int last = 0;
 
 	if (vm == NULL || champ == NULL)
 		return (-1);
+	get_sec_last(vm, champ, &sec, &last);
 	reg = vm->ram[(champ->pc + 2) % MEM_SIZE];
 	if (reg < 1 || reg > REG_NUMBER)
 		return (-1);
