4729eb5ae358d1fef163559b3186e055b0ba64fb
Argann Bay-Eymère <argann.bay-eymere@epitech.eu>
2018-03-08 16:24:18 +0100

    * Fixed a possible error in lldi

--- corewar/src/vm_functions/vm_lldi.c
+++ corewar/src/vm_functions/vm_lldi.c
@@ -18,7 +18,7 @@ int vm_lldi(vm_t *vm, champ_t *champ)
 
 	if (vm == NULL || champ == NULL)
 		return (-1);
-	s = get_indirect_as_int(vm->ram, (champ->pc + ind) % IDX_MOD);
+	s = get_indirect_as_int(vm->ram, (champ->pc + ind));
 	s += sec;
 	reg_start = &(champ->registers[(reg - 1) * REG_SIZE]);
 	for (int i = 0; i < REG_SIZE; i++)
