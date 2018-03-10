02f237f0391cb228138e78af4f016f6dec88f818
mike gerard <mike.gerard27@gmail.com>
2018-03-10 16:50:51 +0100

    change lld and lldi

--- corewar/src/vm_functions/vm_ldi.c
+++ corewar/src/vm_functions/vm_ldi.c
@@ -8,23 +8,48 @@
 #include "corewar.h"
 #include <stdlib.h>
 
-int vm_ldi(vm_t *vm, champ_t *champ)
+void load_ind_direct(vm_t *vm, champ_t *champ)
 {
 	int ind = get_indirect_as_int(vm->ram, (champ->pc + 2) % MEM_SIZE);
+	int reg = vm->ram[(champ->pc + 6) % MEM_SIZE];
+	int s = get_indirect_as_int(vm->ram, (champ->pc + ind % IDX_MOD) \
+% MEM_SIZE);
+	byte_t *reg_start = NULL;
+
+	s += get_indirect_as_int(vm->ram, (champ->pc + 4) % MEM_SIZE);
+	reg_start = &(champ->registers[(reg - 1) * REG_SIZE]);
+	for (int i = 0; i < REG_SIZE; i++)
+		reg_start[i] = vm->ram[(champ->pc + (s + i) % IDX_MOD) \
+% MEM_SIZE];
+	champ->carry = !(champ->carry);
+}
+
+void load_two_direct(vm_t *vm, champ_t *champ)
+{
+	int first = get_indirect_as_int(vm->ram, (champ->pc + 2) % MEM_SIZE);
 	int sec = get_indirect_as_int(vm->ram, (champ->pc + 4) % MEM_SIZE);
 	int reg = vm->ram[(champ->pc + 6) % MEM_SIZE];
-	int s;
+	int s = get_indirect_as_int(vm->ram, champ->pc % MEM_SIZE);
 	byte_t *reg_start = NULL;
+
+	s += first + sec;
+	reg_start = &(champ->registers[(reg - 1) * REG_SIZE]);
+	for (int i = 0; i < REG_SIZE; i++)
+		reg_start[i] = vm->ram[(champ->pc + (s + i) % IDX_MOD) \
+% MEM_SIZE];
+	champ->carry = !(champ->carry);
+}
+
+int vm_ldi(vm_t *vm, champ_t *champ)
+{
+	int type;
 
 	if (vm == NULL || champ == NULL)
 		return (-1);
-	s = get_indirect_as_int(vm->ram, ((champ->pc + ind) % \
-IDX_MOD) % MEM_SIZE);
-	s += sec;
-	reg_start = &(champ->registers[(reg - 1) * REG_SIZE]);
-	for (int i = 0; i < REG_SIZE; i++)
-		reg_start[i] = vm->ram[((champ->pc + s + i) % IDX_MOD) \
-% MEM_SIZE];
-	champ->carry = !(get_register_as_int(reg_start));
+	type = vm->ram[(champ->pc + 1) % MEM_SIZE];
+	if (FIRST_PARAM_TYPE(type) == DIRECT_TYPE)
+		load_two_direct(vm, champ);
+	else
+		load_ind_direct(vm, champ);
 	return (7);
 }
--- corewar/src/vm_functions/vm_lldi.c
+++ corewar/src/vm_functions/vm_lldi.c
@@ -8,21 +8,45 @@
 #include "corewar.h"
 #include <stdlib.h>
 
-int vm_lldi(vm_t *vm, champ_t *champ)
+void load_ind_direct_long(vm_t *vm, champ_t *champ)
 {
-	int ind = get_indirect_as_int(vm->ram, champ->pc + 2);
-	int sec = get_indirect_as_int(vm->ram, champ->pc + 4);
+	int ind = get_indirect_as_int(vm->ram, (champ->pc + 2) % MEM_SIZE);
 	int reg = vm->ram[(champ->pc + 6) % MEM_SIZE];
-	int s;
+	int s = get_indirect_as_int(vm->ram, (champ->pc + ind) % MEM_SIZE);
 	byte_t *reg_start = NULL;
 
-	if (vm == NULL || champ == NULL)
-		return (-1);
-	s = get_indirect_as_int(vm->ram, (champ->pc + ind));
-	s += sec;
+	s += get_indirect_as_int(vm->ram, (champ->pc + 4) % MEM_SIZE);
 	reg_start = &(champ->registers[(reg - 1) * REG_SIZE]);
 	for (int i = 0; i < REG_SIZE; i++)
 		reg_start[i] = vm->ram[(champ->pc + s + i) % MEM_SIZE];
-	champ->carry = !(get_register_as_int(reg_start));
+	champ->carry = !(champ->carry);
+}
+
+void load_two_direct_long(vm_t *vm, champ_t *champ)
+{
+	int first = get_indirect_as_int(vm->ram, (champ->pc + 2) % MEM_SIZE);
+	int sec = get_indirect_as_int(vm->ram, (champ->pc + 4) % MEM_SIZE);
+	int reg = vm->ram[(champ->pc + 6) % MEM_SIZE];
+	int s = get_indirect_as_int(vm->ram, champ->pc % MEM_SIZE);
+	byte_t *reg_start = NULL;
+
+	s += first + sec;
+	reg_start = &(champ->registers[(reg - 1) * REG_SIZE]);
+	for (int i = 0; i < REG_SIZE; i++)
+		reg_start[i] = vm->ram[(champ->pc + s + i) % MEM_SIZE];
+	champ->carry = !(champ->carry);
+}
+
+int vm_lldi(vm_t *vm, champ_t *champ)
+{
+	int type;
+
+	if (vm == NULL || champ == NULL)
+		return (-1);
+	type = vm->ram[(champ->pc + 1) % MEM_SIZE];
+	if (FIRST_PARAM_TYPE(type) == DIRECT_TYPE)
+		load_two_direct_long(vm, champ);
+	else
+		load_ind_direct_long(vm, champ);
 	return (7);
 }
